15958a94229fc762fff33fe9be6a8a0185815ec2
geloprjcts <gelo.prjcts@gmail.com>
2024-06-24 20:14:37 +0800

    Services

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,18 +1,28 @@
 import { Grid, LinearProgress, Card, Typography, Box, Avatar } from "@mui/material";
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import PhoneIcon from '@mui/icons-material/Phone';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import SchoolIcon from '@mui/icons-material/School';
+import WorkIcon from '@mui/icons-material/Work';
 import IMAGE from "../../assets/images";
 
 function About() {
     return (
         <Grid container sx={{ backgroundColor: "pink", display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
             <Card raised={true} sx={{
-                width: '50%',
-                height: 900, display: 'inline-flex',
+                width: '70%',
+                height: 'auto',
+                display: 'inline-flex',
                 flexDirection: 'column',
                 justifyItems: 'center',
-                alignContent: 'center',
-                marginTop: -5,
+                alignItems: 'center',
+                marginY: -10,
                 borderRadius: '50px',
                 padding: 5,
                 backgroundColor: 'lightblue',
@@ -88,7 +98,7 @@ function About() {
                             }} />
                         </Box>
                         <Box sx={{ width: '100%', padding: 1 }}>
-                            <Typography>Git Version Control</Typography>
+                            <Typography>Mobile Development</Typography>
                             <LinearProgress variant="determinate" value={50} sx={{
                                 height: 10,
                                 borderRadius: 5, margin: 1
@@ -103,7 +113,131 @@ function About() {
                         </Box>
                     </Grid>
                 </Grid>
-            </Card>
+                <hr color="black" style={{ width: '100%', marginTop: 2 }} />
+                <Typography variant="h5" textAlign='center' sx={{ fontStyle: 'italic' }}>My Qualification</Typography>
+                <Typography variant="h4" textAlign='center' sx={{ fontWeight: 'bold' }}>Awesome Journey</Typography>
+                <Grid container sx={{marginTop: 5}}>
+                    <Grid item sx={{ width: "40%", display: 'inline-block', justifyContent: 'center', alignContent: 'center', }}>
+                        <Typography variant='h5' sx={{ fontWeight: '800', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+                            <SchoolIcon fontSize="large" />
+                            Education
+                        </Typography>
+                        <Timeline sx={{ marginLeft: -25 }}>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography variant='h5' sx={{ fontWeight: '800' }}>
+                                        Bustos Central Elementary School<br />
+                                    </Typography>
+                                    <Typography variant='caption' sx={{ fontWeight: '300' }}>Elementary</Typography>
+                                    <Box color='#555' sx={{ alignItems: 'center', display: 'flex' }}>
+                                        <CalendarMonthIcon />
+                                        <Typography variant='caption' sx={{ verticalAlign: 'middle', fontWeight: 'bold', fontStyle: 'italic' }}>2006 - 2012</Typography>
+                                    </Box>
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography variant='h5' sx={{ fontWeight: '800' }}>
+                                        Tibagan National High School<br />
+                                    </Typography>
+                                    <Typography variant='caption' sx={{ fontWeight: '300' }}>High School</Typography>
+                                    <Box color='#555' sx={{ alignItems: 'center', display: 'flex' }}>
+                                        <CalendarMonthIcon />
+                                        <Typography variant='caption' sx={{ verticalAlign: 'middle', fontWeight: 'bold', fontStyle: 'italic' }}>2012 - 2016</Typography>
+                                    </Box>
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography variant='h5' sx={{ fontWeight: '800' }}>
+                                        Notre Christi Academy of the Philippines<br />
+                                    </Typography>
+                                    <Typography variant='caption' sx={{ fontWeight: '300' }}>General Academic Strand</Typography>
+                                    <Box color='#555' sx={{ alignItems: 'center', display: 'flex' }}>
+                                        <CalendarMonthIcon />
+                                        <Typography variant='caption' sx={{ verticalAlign: 'middle', fontWeight: 'bold', fontStyle: 'italic' }}>2016 - 2018</Typography>
+                                    </Box>
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography variant='h5' sx={{ fontWeight: '800' }}>
+                                        Bulacan State University<br />
+                                    </Typography>
+                                    <Typography variant='caption' sx={{ fontWeight: '300' }}>Bachelor of Science in Information Technology Major in Web and Mobile Development</Typography>
+                                    <Box color='#555' sx={{ alignItems: 'center', display: 'flex' }}>
+                                        <CalendarMonthIcon />
+                                        <Typography variant='caption' sx={{ verticalAlign: 'middle', fontWeight: 'bold', fontStyle: 'italic' }}>2018 - 2022</Typography>
+                                    </Box>
+                                </TimelineContent>
+                            </TimelineItem>
+                        </Timeline>
+
+                    </Grid>
+                    <Grid item sx={{ width: "40%", display: 'inline-block', justifyItems: 'center', alignContent: 'center', }}>
+                        <Typography variant='h5' sx={{ fontWeight: '800', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+                            <WorkIcon fontSize="large" />
+                            Experience
+                        </Typography>
+                        <Timeline sx={{ marginLeft: -25 }}>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography variant='h5' sx={{ fontWeight: '800' }}>
+                                        iRely, PH<br />
+                                    </Typography>
+                                    <Typography variant='caption' sx={{ fontWeight: 'bold' }}>Junior Application Developer</Typography>
+                                    <Box color='#555' sx={{ alignItems: 'center', display: 'flex' }}>
+                                        <CalendarMonthIcon />
+                                        <Typography variant='caption' sx={{ verticalAlign: 'middle', fontWeight: 'bold', fontStyle: 'italic' }}>2018 - 2022</Typography>
+                                    </Box>
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography variant='h5' sx={{ fontWeight: '800' }}>
+                                        Novalink<br />
+                                    </Typography>
+                                    <Typography variant='caption' sx={{ fontWeight: '300' }}>Non-voice Callcenter Agent</Typography>
+                                    <Box color='#555' sx={{ alignItems: 'center', display: 'flex' }}>
+                                        <CalendarMonthIcon />
+                                        <Typography variant='caption' sx={{ verticalAlign: 'middle', fontWeight: 'bold', fontStyle: 'italic' }}>2018 - 2022</Typography>
+                                    </Box>
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                </TimelineContent>
+                            </TimelineItem>
+                        </Timeline>
+                    </Grid>
+                </Grid>
+            </Card >
         </Grid >
     );
 }
--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -1,15 +1,138 @@
-import { Grid, Container } from "@mui/material";
-
+import { Grid, Box, Card, CardContent, CardMedia, Typography, CardActionArea } from "@mui/material";
+import IMAGE from "../../assets/images";
 function Services() {
     return (
-        <Grid container sx={{ backgroundColor: "brown", display: 'flex', justifyContent: 'space-evenly', alignContent: 'center', marginTop: -5, height: 500 }}>
-            <Grid item sx={{ backgroundColor: "orange", }}>
-                Services World!
+        <Box sx={{ height: 'auto', }}>
+            <Typography variant="h2" sx={{ marginTop: 12, textAlign: 'center' }}>Services</Typography>
+            <Grid container rowSpacing={{ xs: 2, sm: 2, md: 2, xl: 4 }} columnSpacing={{ xs: 4, sm: 4, md: 4, xl: 6 }} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 4 }}>
+                <Grid item >
+                    <Card raised sx={{ maxWidth: 445, borderTopRightRadius: 100 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={IMAGE.Sample2}
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Lizard
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item >
+                    <Card raised sx={{ maxWidth: 445, borderTopRightRadius: 100 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={IMAGE.Sample2}
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Lizard
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item >
+                    <Card raised sx={{ maxWidth: 445, borderTopRightRadius: 100 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={IMAGE.Sample2}
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Lizard
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item>
+                    <Card raised sx={{ maxWidth: 445, borderTopLeftRadius: 100 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={IMAGE.Sample2}
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Lizard
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item>
+                    <Card raised sx={{ maxWidth: 445, borderTopLeftRadius: 100 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={IMAGE.Sample2}
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Lizard
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item>
+                    <Card raised sx={{ maxWidth: 445, borderTopLeftRadius: 100 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={IMAGE.Sample2}
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Lizard
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
             </Grid>
-            <Grid item sx={{ backgroundColor: "yellow", }}>
-                Services World!
-            </Grid>
-        </Grid>
+        </Box>
     );
 }
 
